a97e37857d724f15bede7bc0bcc32c5bd890a888
Victor Marks <Victor@PaulasApleWatch.localdomain>
2018-10-11 16:02:41 -0700

    Finished unit model tests

--- test_user_model.py
+++ test_user_model.py
@@ -8,7 +8,9 @@
 import os
 from unittest import TestCase
 
-from models import db, User, Message, FollowersFollowee
+from models import db, User, Message, FollowersFollowee, Favorite, Bcrypt
+
+bcrypt = Bcrypt()
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
@@ -29,17 +31,30 @@ from app import app
 db.create_all()
 
 
+# FLASK_ENV=production python -m unittest test_user_model.py
+
+
 class UserModelTestCase(TestCase):
     """Test views for messages."""
 
     def setUp(self):
         """Create test client, add sample data."""
-
+        
         User.query.delete()
         Message.query.delete()
         FollowersFollowee.query.delete()
+        Favorite.query.delete()
+
+        db.session.commit()
 
         self.client = app.test_client()
+
+        
+
+    def tearDown(self):
+        """After each test function, delete every table."""
+
+        # :)
 
     def test_user_model(self):
         """Does basic model work?"""
@@ -47,12 +62,25 @@ class UserModelTestCase(TestCase):
         u = User(
             email="test@test.com",
             username="testuser",
-            password="HASHED_PASSWORD"
+            password=bcrypt.generate_password_hash("HASHED_PASSWORD").decode('UTF-8')
+        )
+
+        v = User(
+            email="vest@vest.com",
+            username="vestuser",
+            password=bcrypt.generate_password_hash("HASHED_PASSWORD").decode('UTF-8')
         )
 
         db.session.add(u)
+        db.session.add(v)
+
         db.session.commit()
 
         # User should have no messages & no followers
         self.assertEqual(u.messages.count(), 0)
         self.assertEqual(u.followers.count(), 0)
+        self.assertEqual(u.is_followed_by(v), False)
+        self.assertEqual(u.is_following(v), False)
+        # self.assertIn('testuser', u.authenticate('testuser','HASHED_PASSWORD'))
+
+
